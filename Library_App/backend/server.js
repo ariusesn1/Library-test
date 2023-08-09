5395eed3dbdde308d6ed241aeb0cbadf2b3bd816
Truong2003 <nguyentruongbmt2003@gmail.com>
2023-08-09 16:36:58 +0700

    Crud author and update header, router, ..

--- Library_App/backend/server.js
+++ Library_App/backend/server.js
@@ -4,11 +4,12 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "12345",
+    password: "123456",
     database: "book_management"
 })
 
@@ -16,14 +17,77 @@ app.listen(8081, ()=>{
     console.log("listening");
 })
 
-app.get('/',(re,res)=>{
-    return res.json("From backend");
+app.get('/',(req,res)=>{
+    return res.json("Hello from backend");
 })
 
-app.get('/authors',(re,res)=>{
+app.get("/authors", (req, res) => {
     const sql = "SELECT * FROM authors";
-    db.query(sql,(err,data)=>{
-        if(err) return res.json(err);
+    db.query(sql, (err, data) => {
+        if(err) return res.json("Error");
         return res.json(data);
     })
 })
+
+app.post("/author/create", (req, res) => {
+    const sql = "insert into authors(name, nationality) values(?)";
+    const values = [...Object.values(req.body)];
+    console.log("insert", values);
+    db.query(sql, [values], (err, data) => {
+      console.log(err, data);
+      if (err) return res.json({ error: err.message });
+      else return res.json({ data });
+    });
+});
+
+app.delete("/author/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "DELETE FROM authors WHERE id = ?";
+    
+    db.query(sql, [id], (err, data) => {
+        if (err) {
+            return res.json({ error: err.message });
+        } else {
+            return res.json({ message: "Author deleted successfully" });
+        }
+    });
+});
+
+app.get("/author/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM authors WHERE id = ?";
+    
+    db.query(sql, [id], (err, data) => {
+        if (err) {
+            return res.json({ error: err.message });
+        } else {
+            if (data.length === 0) {
+                return res.json({ message: "Author not found" });
+            }
+            return res.json(data[0]); // Trả về tác giả đầu tiên trong danh sách
+        }
+    });
+});
+
+app.put("/author/update/:id", (req, res) => {
+    const id = req.params.id;
+    console.log("updated " + req.body);
+    const data = req.body;
+    const q =
+      "update authors set " +
+      Object.keys(data)
+        .map((k) => `${k} = ?`)
+        .join(",") +
+      " where id='" +
+      id +
+      "'";
+    console.log(q);
+    db.query(q, [...Object.values(data)], (err, out) => {
+      console.log(err, out);
+      if (err) return res.json({ error: err.message });
+      else {
+        return res.json({ data: out });
+      }
+    });
+  });   
+
---
+++ Library_App/fe/src/Pages/Author/Author.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+export default function Author() {
+
+    const [authors, setAuthors] = useState([])
+
+    useEffect(() => {
+       loadAuthors()
+    }, [])
+
+    const loadAuthors= async ()=>{
+      const result =await axios.get("http://localhost:8081/authors");
+      setAuthors(result.data);
+    }; 
+
+    const deleteAuthor = async (id) => {
+      try {
+        await axios.delete(`http://localhost:8081/author/delete/${id}`);
+        loadAuthors(); // Reload authors after successful delete
+      } catch (error) {
+        console.error('Error deleting author:', error);
+      }
+    };
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='py-4'>
+          <Link className='btn btn-primary mb-3' to="/author/create">CREATE NEW AUTHOR</Link>
+          <table className="table border">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                <th scope="col">Nationality</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {authors.map((data, i) => (
+                <tr key={i}>
+                  <td>{i+1}</td>
+                  <td>{data.name}</td>
+                  <td>{data.nationality}</td>
+                  <td>
+                    <Link to={`/author/update/${data.id }`} className='btn btn-outline-primary mx-2'>Update</Link>
+                    <button className='btn btn-outline-danger'onClick={() => deleteAuthor(data.id)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+}
